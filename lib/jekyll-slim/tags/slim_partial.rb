49bccb5ab0ddf47395ebb0796eb0b1d525bd4dda
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2013-11-28 06:15:06 +0100

    Minor improvements

--- lib/jekyll-slim/tags/slim_partial.rb
+++ lib/jekyll-slim/tags/slim_partial.rb
@@ -2,9 +2,51 @@ require 'slim'
 
 module Jekyll
   class SlimPartialTag < Liquid::Tag
-    def initialize(tag_name, file, tokens)
+
+    SYNTAX_EXAMPLE = "{% slim file.slim param='value' param2='value' %}"
+
+    VALID_SYNTAX = /([\w-]+)\s*=\s*(?:"([^"\\]*(?:\\.[^"\\]*)*)"|'([^'\\]*(?:\\.[^'\\]*)*)'|([\w\.-]+))/ 
+
+    def initialize(tag_name, markup, tokens)
       super
-      @file = file.strip
+      @file, @params = markup.strip.split(' ', 2)
+      validate_params if @params
+    end
+
+    def validate_params
+      full_valid_syntax = Regexp.compile('\A\s*(?:' + VALID_SYNTAX.to_s + '(?=\s|\z)\s*)*\z')
+      unless @params =~ full_valid_syntax
+        raise ArgumentError.new <<-eos
+Invalid syntax for include tag:
+
+      #{@params}
+
+Valid syntax:
+
+      #{SYNTAX_EXAMPLE}
+
+eos
+      end
+    end
+
+    def parse_params(context)
+      params = {}
+      markup = @params
+
+      while match = VALID_SYNTAX.match(markup) do
+        markup = markup[match.end(0)..-1]
+
+        value = if match[2]
+          match[2].gsub(/\\"/, '"')
+        elsif match[3]
+          match[3].gsub(/\\'/, "'")
+        elsif match[4]
+          slim_context[match[4]]
+        end
+
+        params[match[1]] = value
+      end
+      params
     end
 
     def render(context)
@@ -24,12 +66,17 @@ module Jekyll
         choices = Dir['**/*'].reject { |x| File.symlink?(x) }
         if choices.include?(@file)
           source = File.read(@file)
+          context.registers[:site].slim_context.params = parse_params(context.registers[:site].slim_context)
           conversion = ::Slim::Template.new(context.registers[:site].config['slim'].deep_symbolize_keys) { source }.render(context.registers[:site].slim_context)
           partial = Liquid::Template.parse(conversion)
           begin
             return partial.render!(context)
           rescue => e
-            puts "Liquid Exception: #{e.message} in #{self.data["layout"]}"
+            if self.respond_to?(:data)
+              puts "Liquid Exception: #{e.message} in #{self.data["layout"]}"
+            else
+              puts "Liquid Exception: #{e.message} in (Unknown layout)"
+            end
             e.backtrace.each do |backtrace|
               puts backtrace
             end
